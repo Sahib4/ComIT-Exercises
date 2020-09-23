0ac1658ca518a07001dea44472f0e7d534c167dc
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2020-09-23 13:04:31 -0500

    Exercise 19

---
+++ Exercise 19 /index.js
@@ -0,0 +1,14 @@
+var birth_month = "December";
+var first_name = "Sahib"
+var last_name = "Narang"
+
+let city = "Winnipeg";
+let age = 21;
+let next_city = "Calgary";
+
+console.log('Month of birth:',birth_month);
+console.log('first namr:',first_name);
+console.log('last name:',last_name);
+console.log('city:',city);
+console.log('age',age);
+console.log('next city:',next_city);
