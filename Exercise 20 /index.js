8555da6cc9a4efd7e46d880320185ed843e3dc62
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2020-09-25 19:40:57 -0500

    Completed Lesson 20

---
+++ Exercise 20 /index.js
@@ -0,0 +1,20 @@
+let age =21;
+let first_name = "Sahib";
+let course = false;
+let final_score = null;
+
+let winning_number;
+
+console.log("age:", age);
+console.log("name:",name);
+console.log("complete:",course);
+console.log("final score:" ,final_score);
+
+let new_age = String(age);
+console.log("Numeric converted to string:", typeof new_age);
+
+let new_name = Boolean(first_name);
+console.log("Convert string to boolean:", typeof new_name);
+
+let new_course = Number(course);
+console.log("Convert boolean to num:" ,typeof new_course);
