b400fc9f47b8bf9ee4f1273a3f9a935e1a4e0b7f
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2020-09-26 17:19:27 -0500

    Lesson 21

---
+++ Exercise 20 /Exercise 21/index.js
@@ -0,0 +1,18 @@
+alert("welcome");
+
+var number1 = prompt("enter a number");
+
+var number2 = prompt("enter a number");
+
+var a = parseInt(number1);
+var b = parseInt(number2);
+
+if (a>b)
+{
+    alert("confirm");
+}
+else
+
+{
+alert("Not confirm");
+}
