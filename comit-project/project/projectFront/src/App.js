b2f631f805885e955a8ade5e388a1c53e0a0919f
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2020-11-09 15:29:28 -0600

    added cart screen

--- comit-project/project/projectFront/src/App.js
+++ comit-project/project/projectFront/src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route, Link} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import cartScreen from './screens/cartScreen';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+const cart = useSelector((state) => state.cart);
+const {cartItems} = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container"> 
         <header className="row">
             <div className="brand">
-                <a href="/">Shop</a>
+                <Link to="/">Shop</Link>
             </div>
 
             <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign-In</a>
+                <Link to="/cart">Cart
+                {cartItems.length>0 && (
+                  <span className = "badge">{cartItems.length}</span>
+                )}
+                </Link>
+                <Link to="/signin">Sign-In</Link>
             </div>
         </header>
 
         <main>
+          <Route path="/cart/:id?" component={cartScreen}></Route>
           <Route path = "/product/:id" component ={ProductScreen}></Route>
           <Route path="/" component = {HomeScreen} exact></Route>
-
         </main>
 
         <footer className="row center"> All rights resvered </footer>
